12adeb1e53fc74aaf49866cf9c5da1c19532e972
hubessfatma <thef7.marketing@gmail.com>
2023-02-21 11:22:39 -0800

    poui

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 /**
+ * print_alphabet -  a function that prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
 
-*print_alphabet - a function that prints the alphabet in lowercase
-*Return: Always 0
-*/
 void print_alphabet(void)
 {
-char letter = 'a';
+        char letter = 'a';
 
-while (letter <= 'z
-{
-_putchar(letter);
-letter++;
-}
-        
-_putchar('\n');
+        while (letter <= 'z')
+        {
+                _putchar(letter);
+                letter++;
+        }
+        _putchar('\n');
 }
