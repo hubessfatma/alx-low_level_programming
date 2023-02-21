c5cd501c2a3c9dc5e7566777615da96db590299b
hubessfatma <thef7.marketing@gmail.com>
2023-02-21 11:47:47 -0800

    kill

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+ *
+ * Return: ALways 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
+Footer
+© 2023 GitHub, Inc.
