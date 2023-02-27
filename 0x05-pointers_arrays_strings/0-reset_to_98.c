28775901f2d7be342e4f8faef96c3de59f421b4b
hubessfatma <thef7.marketing@gmail.com>
2023-02-27 10:42:45 -0800

    pop

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,8 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * reset_to_98 - updates the value *n points to.
- * @n: the number to modify.
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
  */
-
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
